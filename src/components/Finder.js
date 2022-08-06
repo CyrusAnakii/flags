15258881b15611a917b1ca88da74edcc12dd1f6c
Matt Ramsey <cairsiden@yahoo.com>
2022-08-06 03:59:55 -0400

    Prototyped Search by Yellow Color

--- src/components/Finder.js
+++ src/components/Finder.js
@@ -1,21 +1,44 @@
 import { Card } from "react-bootstrap";
 import flagArray from "../MasterList";
 import { createRoot } from "react-dom/client";
+import yellowState from "./HomePage";
 
 function Finder() {
-  const flagParade = flagArray.map((result) => (
-    <div className="col-1">
-      <Card key={result.id}>
-        <Card.Link href={result.link}>
-          <Card.Img src={result.image} className="mx-auto" />
-          <Card.Title className="text-center">{result.name}</Card.Title>
-        </Card.Link>
-      </Card>
-    </div>
-  ));
-  const container = document.getElementById("space");
-  const root = createRoot(container);
-  root.render(flagParade);
+  if (yellowState) {
+    const yellowResults = flagArray.filter((e) => {
+      return e.colors.includes("Yellow");
+    });
+    console.log(yellowResults);
+    const flagParade = yellowResults.map((result) => (
+      <div className="col-1">
+        <Card key={result.id}>
+          <Card.Link href={result.link}>
+            <Card.Img src={result.image} className="mx-auto" />
+            <Card.Title className="text-center">{result.name}</Card.Title>
+          </Card.Link>
+        </Card>
+      </div>
+    ));
+    const container = document.getElementById("space");
+    const root = createRoot(container);
+    root.render(flagParade);
+  }
 }
+
+// function Finder() {
+//   const flagParade = flagArray.map((result) => (
+//     <div className="col-1">
+//       <Card key={result.id}>
+//         <Card.Link href={result.link}>
+//           <Card.Img src={result.image} className="mx-auto" />
+//           <Card.Title className="text-center">{result.name}</Card.Title>
+//         </Card.Link>
+//       </Card>
+//     </div>
+//   ));
+//   const container = document.getElementById("space");
+//   const root = createRoot(container);
+//   root.render(flagParade);
+// }
 
 export default Finder;
